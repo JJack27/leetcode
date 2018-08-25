aa43c81c89ce358bb719dc5638a787b26f8d16e4
Yizhou Zhao <yizhouzhao@Jacks-MBP.vc.shawcable.net>
2018-08-25 09:55:58 -0700

    finished question 13

--- 13_RomanToInteger.py
+++ 13_RomanToInteger.py
@@ -1,7 +1,26 @@
 class Solution:
+    trans = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        
+        total = 0
+        skip_next = False
+        for i in range(len(s)):
+            if not skip_next:
+                if(i < len(s)-1 ):
+                    if self.compare(s[i], s[i+1]):
+                        total += self.trans[s[i+1]] - self.trans[s[i]]
+                        skip_next = True
+                    else:
+                        total += self.trans[s[i]]
+                else:
+                    total += self.trans[s[i]]
+            else:
+                skip_next = False
+        return total
+
+    
+    def compare(self, s1, s2):
+        return self.trans[s1] < self.trans[s2]
--- 3_LongestSubstring.py
+++ 3_LongestSubstring.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        substring = ""
+        maximum = 0
+
+        for i in range(len(s)):
+            substring = s[i]
+            for j in range(i+1,len(s)):
+                if s[j] not in substring:
+                    substring += s[j]
+                    subLength = len(substring)
+                    if subLength > maximum:
+                        maximum = subLength
+                else:
+                    subLength = len(substring)
+                    if subLength > maximum:
+                        maximum = subLength
+                    break
+            subLength = len(substring)
+            if subLength > maximum:
+                maximum = subLength
+
+        return maximum
