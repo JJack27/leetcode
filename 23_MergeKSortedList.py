4e2d3bdd7832f95fc6ccfbe7b8a95c1cde46572a
JJack27 <yizhouzhao27@gmail.com>
2018-08-25 22:40:35 -0700

    add question 19

--- 23_MergeKSortedList.py
+++ 23_MergeKSortedList.py
@@ -10,33 +10,13 @@ class Solution:
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        first = ListNode(None)
-        pointer = first
-        min_value = 0
-        min_index = 0
-        num_pop = 0
-        new_list = []
-        for i in range(len(lists)):
-            if (lists[i] != None):
-                new_list.append(lists[i])
-                continue
-
-        while new_list != []:
-            min_value = new_list[0].val
-            min_index = 0
-            # find the minimal value and its index
-            for i in range(len(new_list)):
-                if(new_list[i].val < min_value):
-                    min_value = new_list[i].val
-                    min_index = i
-            new_list[min_index] = new_list[min_index].next
-            if (new_list[min_index] == None):
-                new_list.pop(min_index)
-            pointer.val = min_value
-            if(new_list != []):
-                temp = ListNode(None)
-                pointer.next = temp
-                pointer = temp
-        if (first.val == None):
-            return []
-        return first
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
